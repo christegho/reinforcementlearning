c5126ea77837fa0aed3d3984e67d513ecf4f04ec
C. Tegho <ct506@mlsalt-cpu1.eng.cam.ac.uk>
2017-02-13 01:23:30 +0000

    working on sarsa

--- policyIteration.m
+++ policyIteration.m
@@ -6,29 +6,32 @@ v = zeros(1,nstates);
 v_ = zeros(1,nstates);
 
 pi = ones(nstates,1);
-
+maxiter = 0
 it2 = 0;
 piStable = false;
 
-while( it2 < 100)
-sprintf('one more it')
-pi = ones(nstates,1);
+while( it2 < maxit)
+
     %policy evaluation  
+   
+   
+diff = inf;
+it=0;
+while (diff > tol && it < maxit)
+    maxiter = max(it,maxiter);
+
     for state= 1:nstates
-        sprintf('here---')
         P = reshape(model.P(state,:,:), size(model.P,2),size(model.P,3));
         R = repmat(model.R(state,:), nstates,1);
-        v_old = inf;
-        it = 0 ;
-        while (abs(v_(state)-v_old) > tol && it < 1)
-            sprintf('here')
-            it = it+1;
-            v_old = v_(state);
-            v_(state) = sum(pi(state)*(sum(P.*(R+model.gamma*repmat(v, size(R,2),1)'))));
-        end
-        it
+        pi_prob = zeros(1,size(R,2));
+            pi_prob(pi(state)) = 1;
+        v_(state)  = sum(pi_prob.*(sum(P.*(R+model.gamma*repmat(v, size(R,2),1)'))));
+        
     end
-    
+    it = it+1;
+    diff = norm(v-v_,3);
+    v = v_
+end
 %policy improvement
 piStable = true;
 for state2= 1:nstates
@@ -36,14 +39,13 @@ for state2= 1:nstates
     P2 = reshape(model.P(state2,:,:), size(model.P,2),size(model.P,3));
     R2 = repmat(model.R(state2,:), nstates,1);
     pi_old = pi(state2);
-    [A, I] = max(sum(P2.*(R2+model.gamma*repmat(v_, size(R2,2),1)')));
+    [A, I] = max(sum(P2.*(R2+model.gamma*repmat(v, size(R2,2),1)')));
     pi(state2) = I;
         if (pi(state2) ~= pi_old)
             piStable = false;        
         end
         
 end
-v = v_
 it2 = it2+1;
 
 end
--- valueIteration.m
+++ valueIteration.m
@@ -3,21 +3,24 @@ function [v, pi] = valueIteration(model, maxit)
 tol = 0.0001;
 nstates = size(model.P,1);
 v = zeros(1,nstates);
+v_ = zeros(1,nstates);
 pi = zeros(nstates,1);
 maxiter = 0;
-for state= 1:nstates
-    P = reshape(model.P(state,:,:), size(model.P,2),size(model.P,3));
-    R = repmat(model.R(state,:), nstates,1);
-    v_old = inf;
-    it = 0 ;
-    while (abs(v(state)-v_old) > tol && it < maxit)
-       it = it+1;
-       v_old = v(state);
-       maxiter = max(it,maxiter);
-       [A, I] = max(sum(P.*(R+model.gamma*repmat(v, size(R,2),1)')));
-       v(state) = A;
+diff = inf;
+it=0;
+while (diff > tol && it < maxit)
+    maxiter = max(it,maxiter);
+
+    for state= 1:nstates
+        P = reshape(model.P(state,:,:), size(model.P,2),size(model.P,3));
+        R = repmat(model.R(state,:), nstates,1);
+        [A, I] = max(sum(P.*(R+model.gamma*repmat(v, size(R,2),1)')));
+        v_(state) = A;
+        pi(state) = I;
     end
-    pi(state) = I;
+    it = it+1;
+    diff = norm(v-v_,3);
+    v = v_
 end
 maxiter
 end
